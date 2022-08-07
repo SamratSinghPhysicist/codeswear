d4a9493fb8e5d2571a535ee49b4b982fd34b86e5
Samrat <101398996+SamratFF@users.noreply.github.com>
2022-08-07 13:29:00 +0530

    Designing Cart Sidebar
    Cart sidebar has been designed till now

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,34 @@
-import React from "react";
+import { React, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+import { BsFillBagCheckFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [displayToggleCart, setDisplayToggleCart] = useState('none')
+
+  // const toggleCart = () => {
+  //   if (ref.current.classList.contains('translate-x-full')) {
+  //     ref.current.classList.remove('translate-x-full')
+  //     ref.current.classList.add('translate-x-0')
+  //   }
+  //   else if (!ref.current.classList.contains('translate-x-full')) {
+  //     ref.current.classList.remove('translate-x-0')
+  //     ref.current.classList.add('translate-x-full')
+  //   }
+  // }
+
+  const toggleCartDisplay = () => {
+    if (displayToggleCart === 'none') {
+      setDisplayToggleCart('block')
+    } else if (displayToggleCart === 'block') {
+      setDisplayToggleCart('none')
+    }
+  }
+
+  const ref = useRef()
+
   return (
     <>
       <header>
@@ -46,8 +70,23 @@ const Navbar = () => {
               </Link>
             </ul>
           </div>
-          <div className="cart absolute right-0 top-4 mx-5">
+
+          <div onClick={toggleCartDisplay} className="cart absolute right-0 top-4 mx-5 cursor-pointer">
             <AiOutlineShoppingCart className="text-2xl md:text-3xl" />
+          </div>
+
+          <div ref={ref} style={{ display: displayToggleCart }} className="sideCart w-72 h-full absolute top-0 right-0 bg-pink-100 py-10 px-10 z-10 ease-in-out duration-1000 transform transition-transform">
+            <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
+            <span onClick={toggleCartDisplay} className="absolute top-5 right-4 text-2xl cursor-pointer text-pink-500"><AiFillCloseCircle /></span>
+            <ol className="list-decimal font-bold">
+              <li>
+                <div className="item flex my-5 mx-2">
+                  <div className="font-semibold w-2/3">TShrits - Wear the Code</div>
+                  <div className="font-semibold w-1/3 flex items-center justify-center text-lg"> <AiFillMinusCircle className="cursor-pointer text-pink-500" /> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-pink-500" /></div>
+                </div>
+              </li>
+            </ol>
+            <button className="flex mt-10 text-white bg-pink-500 border-0 py-[0.35rem] px-5 focus:outline-none hover:bg-pink-600 rounded text-lg items-center justify-center space-x-1"><BsFillBagCheckFill /> <span className="text-base">Checkout</span></button>
           </div>
         </div>
       </header>
